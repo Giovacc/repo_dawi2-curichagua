06efddb90e7e520b97dc9cf9d850e3507f54d8aa
Giovacc <i201811506@cibertec.edu.pe>
2021-05-23 15:23:54 -0500

    primer commit

---
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -0,0 +1,47 @@
+package com.empresa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.empresa.entity.Alumno;
+import com.empresa.service.AlumnoService;
+
+import lombok.extern.apachecommons.CommonsLog;
+import lombok.extern.java.Log;
+
+@CommonsLog
+@RestController
+@RequestMapping("/rest/alumno")
+public class AlumnoController {
+
+	@Autowired
+	private AlumnoService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Alumno>> lista(){
+//		Log.info(">>>> lista ");
+		System.out.println(">>>> lista ");
+		List<Alumno> lstAlumno = service.listaAlumno();
+		return ResponseEntity.ok(lstAlumno);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Alumno> registra(@RequestBody Alumno Obj){
+//		Log.info(">>>> registra " + Obj.getIdAlumno());
+		System.out.println(">>>> registra " + Obj.getIdAlumno());
+		Alumno ObjSalida = service.insertaActualizaAlumno(Obj);
+		if (ObjSalida != null) {
+			return ResponseEntity.ok(ObjSalida);
+		}else {
+			return ResponseEntity.badRequest().build();
+		}
+		
+	}
+}
